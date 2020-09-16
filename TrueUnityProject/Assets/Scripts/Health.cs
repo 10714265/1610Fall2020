75960d1f6874b1fe91a414d5b71a3db3a1c0819a
Parker <10714265@my.uvu.edu>
2020-09-16 08:41:37 -0600

    Sep 16

---
+++ TrueUnityProject/Assets/Scripts/Health.cs
@@ -0,0 +1,14 @@
+﻿
+using System;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+   public int health;
+
+   private void OnTriggerEnter(Collider other)
+   {
+      health++;
+      print(health);
+   }
+}
