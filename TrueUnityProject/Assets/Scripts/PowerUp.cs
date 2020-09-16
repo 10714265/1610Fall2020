d395fbe2618df312d6049b575a769933ad3bcd59
Parker <10714265@my.uvu.edu>
2020-09-15 23:07:01 -0600

    UnityUpdate-9/15-4

---
+++ TrueUnityProject/Assets/Scripts/PowerUp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUp : MonoBehaviour
+{
+    public int score;
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        score++;
+        print(score);
+    }
+}
