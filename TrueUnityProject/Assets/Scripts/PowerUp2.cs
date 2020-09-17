cbdce04ee67a7e531ab1cca957e2a131d52a849e
Parker <10714265@my.uvu.edu>
2020-09-17 14:33:02 -0600

    9/17

---
+++ TrueUnityProject/Assets/Scripts/PowerUp2.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUp2 : MonoBehaviour
+{
+    public int value;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        value++;
+        print(value);
+    }
+}
