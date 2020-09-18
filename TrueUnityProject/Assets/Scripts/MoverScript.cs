6a37a2c2978ceb31c728ab58078783f20ad5709c
Parker <10714265@my.uvu.edu>
2020-09-17 20:08:28 -0600

    9/17

--- TrueUnityProject/Assets/Scripts/MoverScript.cs
+++ TrueUnityProject/Assets/Scripts/MoverScript.cs
@@ -1,32 +1,23 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MoverScript : MonoBehaviour
 {
-    public float speed = 5f;
-    public int score = 100;
-
-    public float health = 100;
-    public float password;
-    public string playername;
-    public int points = 0/100;
-
+    public float speed = 10f;
+    public int count;
+    public Vector3 rotationVector3;
     private void Update()
     {
-        var myInput = speed * Time.deltaTime * Input.GetAxis("Vertical");
-        var urInput = speed * Time.deltaTime * Input.GetAxis("Horizontal");
-        transform.Translate(urInput, myInput, 0);
-    }
-
-    public void Up()
-    {
-        transform.Translate(0,speed, 0);
-    }
-    
-    public void Down()
-    {
-        transform.Translate(0,-speed, 0);
+        count++;
+        if (count > 100)
+        {
+            print("Greater");
+        }
+        
+        rotationVector3.y = speed * Time.deltaTime;
+        transform.Rotate(rotationVector3);
     }
 }
 
