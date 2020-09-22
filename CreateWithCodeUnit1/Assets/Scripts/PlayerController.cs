e0cd3e067a5e6926032ba6af1e2ee70b89067093
Parker <10714265@my.uvu.edu>
2020-09-21 22:32:16 -0600

    9/21

--- CreateWithCodeUnit1/Assets/Scripts/PlayerController.cs
+++ CreateWithCodeUnit1/Assets/Scripts/PlayerController.cs
@@ -13,6 +13,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // Move the vehicle forward
+        transform.Translate(0, 0, 1);
     }
 }
