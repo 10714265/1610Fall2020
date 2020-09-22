8225a4c7a78447d24bc4e554abf5c818d79d756f
Parker <10714265@my.uvu.edu>
2020-09-21 19:47:36 -0600

    9/21

--- TrueUnityProject/Assets/Scripts/PowerUp.cs
+++ TrueUnityProject/Assets/Scripts/PowerUp.cs
@@ -2,14 +2,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class PowerUp : MonoBehaviour
 {
     public int score;
+    public UnityEvent powerUpEvent;
     
     private void OnTriggerEnter(Collider other)
     {
         score++;
         print(score);
+        powerUpEvent.Invoke();
     }
 }
---
+++ TrueUnityProject/Assets/Scripts/UnityEvents.cs
@@ -0,0 +1,13 @@
+﻿
+using System;
+using UnityEngine;
+using UnityEngine.Events;
+public class UnityEvents : MonoBehaviour
+{
+    public UnityEvent myUnityEvent;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        myUnityEvent.Invoke(); 
+    }
+}
