30276469bfda1a5a1b4064248215c48c8675a4af
Parker <10714265@my.uvu.edu>
2020-09-23 22:10:19 -0600

    9/23

--- CreateWithCodeUnit1/Assets/Scripts/PlayerController.cs
+++ CreateWithCodeUnit1/Assets/Scripts/PlayerController.cs
@@ -14,7 +14,7 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         // Move the vehicle forward
-        transform.Translate(0, 0, 1);
+        transform.Translate(Vector3.forward * Time.deltaTime * 20);
         
     }
 }
---
+++ TrueUnityProject/Assets/Scripts/CharacterMover.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMover : MonoBehaviour
+{
+    public CharacterController controller;
+    public Vector3 moveDirection, movement;
+    public float moveSpeed = 3f, gravity = -9.81f;
+    
+        // Start is called before the first frame update
+        private void Update()
+        {
+            var moveSpeed
+            moveDirection.Set(moveSpeed, 0, 0);
+            movement = moveDirection * Time.deltaTime;
+            controller.Move(moveDirection);
+        }
+        
+}
