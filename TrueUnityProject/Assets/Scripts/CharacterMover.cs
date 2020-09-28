887a49466f874745b8847982b81f8e4e2372f908
Parker <10714265@my.uvu.edu>
2020-09-28 11:11:08 -0600

    9/28

--- TrueUnityProject/Assets/Scripts/CharacterMover.cs
+++ TrueUnityProject/Assets/Scripts/CharacterMover.cs
@@ -1,20 +1,39 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CharacterMover : MonoBehaviour
 {
-    public CharacterController controller;
-    public Vector3 moveDirection, movement;
-    public float moveSpeed = 3f, gravity = -9.81f;
-    
-        // Start is called before the first frame update
-        private void Update()
-        {
-            var moveSpeed
-            moveDirection.Set(moveSpeed, 0, 0);
-            movement = moveDirection * Time.deltaTime;
-            controller.Move(moveDirection);
-        }
-        
+   public CharacterController controller;
+   public float moveSpeed = 5f, gravity = -9.81f, jumpForce = 10f;
+   
+   
+   
+   private Vector3 moveDirection;
+   private float yDirection;
+
+
+   private void Update()
+   {
+      var moveSpeedInput = moveSpeed * Input.GetAxis("Horixontal");
+
+      yDirection += gravity * Time.deltaTime;
+
+      if (controller.isGrounded && moveDirection.y < 0)
+      {
+         yDirection = -1f;
+      }
+
+      if (Input.GetButtonDown("Jump"))
+      {
+         yDirection = jumpForce;
+      }
+      
+      moveDirection.Set(moveSpeedInput,0,0);
+      controller.Move(moveDirection*Time.deltaTime);
+
+      var movement = moveDirection * Time.deltaTime;
+      controller.Move(movement);
+   }
 }
