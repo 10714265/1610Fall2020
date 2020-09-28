f33392e01b8287d7034d6ff23bdc63d7198a492d
Parker <10714265@my.uvu.edu>
2020-09-28 12:25:37 -0600

    9/28

--- CreateWithCodeUnit1/Assets/Scripts/PlayerMover.cs
+++ CreateWithCodeUnit1/Assets/Scripts/PlayerMover.cs
@@ -2,20 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class PlayerMover : MonoBehaviour
 {
-   public float speed = 5.0f;
+    public GameObject player;
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
+
+   
+
+}
 
     // Update is called once per frame
     void Update()
     {
-        // Move the vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
-        
+        transform.position = player.transform.position;
     }
 }
