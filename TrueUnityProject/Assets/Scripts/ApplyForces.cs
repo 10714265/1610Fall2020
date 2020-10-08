d924d8057a6ebc606a53d3d7aaa84b80a9f4ed64
Parker <10714265@my.uvu.edu>
2020-10-08 15:27:24 -0600

    FlappyBird 10/8

---
+++ TrueUnityProject/Assets/Scripts/ApplyForces.cs
@@ -0,0 +1,18 @@
+﻿
+using UnityEngine;
+
+public class ApplyForces : MonoBehaviour
+{
+
+    public Rigidbody rBody;
+    public float force = 100f;
+    
+    
+    // Start is called before the first frame update
+    void OnMouseDown()
+    {
+        rBody.AddForce(0, force, 0);
+    }
+
+   
+}
---
+++ TrueUnityProject/Assets/Scripts/Instancer.cs
@@ -0,0 +1,24 @@
+﻿
+using System.Collections;
+using UnityEngine;
+
+public class Instancer : MonoBehaviour
+{
+    public GameObject prefab;
+    public float delay = 2f;
+    
+    
+    IEnumerator Start()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(delay);
+            Instantiate(prefab);  
+            
+        }
+        
+        
+    }
+
+   
+}
