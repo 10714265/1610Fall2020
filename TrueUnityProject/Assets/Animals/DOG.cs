1cdeef849293dd839d9691e93bf0d01ac1eb1c93
Parker <10714265@my.uvu.edu>
2020-10-08 18:06:53 -0600

    Animal 10/8

---
+++ TrueUnityProject/Assets/Animals/DOG.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DOG : Mammal
+{
+   public float barkVolume = 1f;
+}
---
+++ TrueUnityProject/Assets/Animals/Mammal.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mammal : Animal
+{
+   public Color hairColor = Color.green;
+   public int hairCount = 1000;
+
+}
