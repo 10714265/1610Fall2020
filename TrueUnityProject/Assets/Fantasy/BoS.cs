9c0b5c43dac981216356dc028d8f92a9e59919bc
Parker <10714265@my.uvu.edu>
2020-10-08 18:58:00 -0600

    Rpg 10/8

---
+++ TrueUnityProject/Assets/Fantasy/BoS.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoS : Pc
+{
+   public string powerarmor = "T-45";
+}
---
+++ TrueUnityProject/Assets/Fantasy/Pc.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pc : MonoBehaviour
+{
+   public float hitPoints = 100f;
+   public float actionPoints = 100f;
+   
+   public int strength = 10;
+   public int perception = 10;
+   public int endurance = 10;
+   public int charisma = 10;
+   public int intelliegence = 10;
+   public int agility = 10;
+   public int luck = 10;
+
+   protected void Move()
+   {
+      
+   }
+
+}
+
