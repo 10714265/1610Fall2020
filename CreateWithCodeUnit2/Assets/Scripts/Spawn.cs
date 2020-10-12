44a079d783adfc5cf6c6d8453a5ff0f66559e9d4
Parker <10714265@my.uvu.edu>
2020-10-12 15:55:31 -0600

    CWC 10/12

---
+++ CreateWithCodeUnit2/Assets/Scripts/Spawn.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour
+{
+    public GameObject[] animalPrefabs;
+    private float spawnRangex = 20;
+    private float spawnPosZ = 20;
+    
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            SpawnRandomAnimal();
+            
+        }
+           
+    }
+
+    void SpawnRandomAnimal()
+    {
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangex, spawnRangex), 0, spawnPosZ);
+        Instantiate(animalPrefabs[animalIndex], spawnPos,
+            animalPrefabs[animalIndex].transform.rotation); 
+    }
+}
