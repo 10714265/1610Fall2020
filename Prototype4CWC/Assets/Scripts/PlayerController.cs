bb4160288b0dc9e5d5d68a7b783ff72b153dae0a
Parker <10714265@my.uvu.edu>
2020-10-17 15:02:40 -0600

    CWC 4 ball

--- Prototype4CWC/Assets/Scripts/PlayerController.cs
+++ Prototype4CWC/Assets/Scripts/PlayerController.cs
@@ -18,9 +18,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
         float forwardInput = Input.GetAxis("Vertical");
-            
         playerRb.AddForce(focalPoint.transform.forward * forwardInput * speed);    
     }
 }
