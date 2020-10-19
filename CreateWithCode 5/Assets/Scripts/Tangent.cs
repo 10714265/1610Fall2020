ab306fccf115ca6ff70cdfe79239a7e002d43088
Parker <10714265@my.uvu.edu>
2020-10-19 17:57:43 -0600

    CWC 5 Prefab

--- CreateWithCode 5/Assets/Scripts/Tangent.cs
+++ CreateWithCode 5/Assets/Scripts/Tangent.cs
@@ -4,10 +4,17 @@ using UnityEngine;
 
 public class Tangent : MonoBehaviour
 {
+    private Rigidbody targetRb;
     // Start is called before the first frame update
     void Start()
     {
+        targetRb = GetComponent<Rigidbody>();
         
+        targetRb.AddForce(Vector3.up*Random.Range(12, 16), ForceMode.Impulse);
+        targetRb.AddTorque(Random.Range(-10, 10), Random.Range(-10, 10),Random.Range(-10, 10),ForceMode.Impulse);
+        
+        transform.position = new Vector3(Random.Range(-4,4), -6);
+
     }
 
     // Update is called once per frame
