b2cdb115b728cdfeb14cb20f3dc0db43d791eec4
Parker <10714265@my.uvu.edu>
2020-10-20 09:40:17 -0600

    CWC 5 GameManager

--- CreateWithCode 5/Assets/Scripts/Tangent.cs
+++ CreateWithCode 5/Assets/Scripts/Tangent.cs
@@ -5,15 +5,22 @@ using UnityEngine;
 public class Tangent : MonoBehaviour
 {
     private Rigidbody targetRb;
+    private float minSpeed = 12;
+    private float maxSpeed = 16;
+    private float maxTorque = 10;
+    private float xRange = 4;
+    private float ySpawnPos = -6;
+        
+        
     // Start is called before the first frame update
     void Start()
     {
         targetRb = GetComponent<Rigidbody>();
         
-        targetRb.AddForce(Vector3.up*Random.Range(12, 16), ForceMode.Impulse);
-        targetRb.AddTorque(Random.Range(-10, 10), Random.Range(-10, 10),Random.Range(-10, 10),ForceMode.Impulse);
-        
-        transform.position = new Vector3(Random.Range(-4,4), -6);
+        targetRb.AddForce(RandomForce(), ForceMode.Impulse);
+        targetRb.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(),ForceMode.Impulse);
+
+        transform.position = RandomSpawnPos();
 
     }
 
@@ -21,5 +28,20 @@ public class Tangent : MonoBehaviour
     void Update()
     {
         
+    }
+
+    Vector3 RandomForce()
+    {
+        return Vector3.up * Random.Range(minSpeed, maxSpeed);
+    }
+
+    float RandomTorque()
+    {
+        return Random.Range(-maxTorque, maxTorque);
+    }
+
+    Vector3 RandomSpawnPos()
+    {
+        return new Vector3(Random.Range(-xRange, xRange), ySpawnPos);
     }
 }
