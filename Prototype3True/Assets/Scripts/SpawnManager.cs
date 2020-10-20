dd2a76c4ddeebfa4b2b681a1e6c27de44a20ed65
Parker <10714265@my.uvu.edu>
2020-10-20 14:04:42 -0600

    Game Over CWC

--- Prototype3True/Assets/Scripts/SpawnManager.cs
+++ Prototype3True/Assets/Scripts/SpawnManager.cs
@@ -10,9 +10,12 @@ public class SpawnManager : MonoBehaviour
     private float startDelay = 2;
 
     private float repeatRate = 2;
+
+    private PlayerController playerControllerScript;
     // Start is called before the first frame update
     void Start()
     {
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
         InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
     }
 
@@ -24,6 +27,10 @@ public class SpawnManager : MonoBehaviour
 
     void SpawnObstacle()
     {
-        Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+        if (playerControllerScript.gameOver == false)
+        {
+            Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+        }
+        
     }
 }
