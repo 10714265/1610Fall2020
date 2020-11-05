3663038dc43fe212dcabd3a90f236d43383b4f2b
Parker <10714265@my.uvu.edu>
2020-11-05 14:19:09 -0700

    UnityProject2 powerup

---
+++ UnityProject2/Assets/HealthUp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthUp : MonoBehaviour
+{
+    public int healthMy = 100;
+    private void OnTriggerEnter(Collider other)
+    {
+        healthMy++;
+        print(healthMy);
+    }
+}
---
+++ UnityProject2/Assets/Scripts/PowerUp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUp : MonoBehaviour
+{
+    public int scoreboi;
+    private void OnTriggerEnter(Collider other)
+    {
+        scoreboi++;
+        print(scoreboi);
+    }
+}
