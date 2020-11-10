806805d7d6dd9efdd337b5004fc8c0f72ca0d0f6
Parker <10714265@my.uvu.edu>
2020-11-10 14:32:56 -0700

    UnityProject 2 Prefabs

---
+++ UnityProject2/Assets/Scripts/Destroy.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy : MonoBehaviour
+{
+   private void OnCollisionEnter(Collision other)
+   {
+      Destroy(other.gameObject);
+   }
+}
---
+++ UnityProject2/Assets/Scripts/Instan.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instan : MonoBehaviour
+{
+    public GameObject prefab;
+
+    public float delay;
+    // Start is called before the first frame update
+    IEnumerator Start()
+    {
+        while (true)
+        {
+            Instantiate(prefab);
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
