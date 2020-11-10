be32e24ea837672b6087fd598d12f8941e41f411
Parker <10714265@my.uvu.edu>
2020-11-09 20:36:36 -0700

    UnityProject2 jump

--- UnityProject2/Assets/Scripts/CharacterMover.cs
+++ UnityProject2/Assets/Scripts/CharacterMover.cs
@@ -5,12 +5,24 @@ using UnityEngine;
 public class CharacterMover : MonoBehaviour
 {
     public CharacterController mover;
-    public Vector3 direction;
+    public Vector3 direction, movement;
     public float moveSpeed = 3f;
+    public float gravity = -9.81f;
+    public float jumpForce = 30f;
 
     private void Update()
     {
-        direction.Set(moveSpeed, 0, 0);
-        mover.Move(direction);
+        var movespeedInput = moveSpeed * Input.GetAxis("Horizontal");
+        direction.Set(movespeedInput, gravity, 0);
+       
+        if (Input.GetButtonDown("Jump"))
+        {
+            direction.y += jumpForce;
+        }
+        
+        movement = direction * Time.deltaTime;
+        mover.Move(movement);
+
+        
     }
 }
