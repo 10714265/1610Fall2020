12d3fd60c2ef31389055d4acdfebf5ebf00a3927
Parker <10714265@my.uvu.edu>
2020-11-10 13:07:56 -0700

    UnityProject2 rigidbody

---
+++ UnityProject2/Assets/Scripts/ApplyForce.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ApplyForce : MonoBehaviour
+{
+    public Rigidbody theBody;
+    public float theForce = 30f;
+
+    void start()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        theBody.AddForce( 0 , theForce, 0);
+    }
+}
--- UnityProject2/Assets/Scripts/CharacterMover.cs
+++ UnityProject2/Assets/Scripts/CharacterMover.cs
@@ -6,18 +6,27 @@ public class CharacterMover : MonoBehaviour
 {
     public CharacterController mover;
     public Vector3 direction, movement;
-    public float moveSpeed = 3f;
+    public float moveSpeed = 3f, moveupDown = 3f;
     public float gravity = -9.81f;
     public float jumpForce = 30f;
 
+    public float yDirection;
     private void Update()
     {
         var movespeedInput = moveSpeed * Input.GetAxis("Horizontal");
-        direction.Set(movespeedInput, gravity, 0);
+        var moveupdownInput = moveupDown * Input.GetAxis("Vertical");
+        direction.Set(movespeedInput, yDirection, moveupdownInput);
+
+        yDirection += gravity * Time.deltaTime;
+
+        if (mover.isGrounded && direction.y < 0)
+        {
+            yDirection = -1f;
+        }
        
         if (Input.GetButtonDown("Jump"))
         {
-            direction.y += jumpForce;
+            yDirection = jumpForce;
         }
         
         movement = direction * Time.deltaTime;
