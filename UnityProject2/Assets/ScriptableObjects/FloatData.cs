0615282dcb119ccf9eff78f493252b5133a80bc2
Parker <10714265@my.uvu.edu>
2020-11-13 22:41:31 -0700

    UnitProject2 Health

---
+++ UnityProject2/Assets/ScriptableObjects/FloatData.cs
@@ -0,0 +1,12 @@
+﻿
+using UnityEngine;
+[CreateAssetMenu]
+public class FloatData : ScriptableObject
+{
+   public float value;
+
+   public void UpdateValue(int number)
+   {
+      value += number;
+   }
+}
--- UnityProject2/Assets/Scripts/TriggerEvents.cs
+++ UnityProject2/Assets/Scripts/TriggerEvents.cs
@@ -1,9 +1,16 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿
+using System;
 using UnityEngine;
 using UnityEngine.Events;
 
 public class TriggerEvents : MonoBehaviour
 {
     public UnityEvent triggerEnterEvent;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        triggerEnterEvent.Invoke();
+    }
 }
+
