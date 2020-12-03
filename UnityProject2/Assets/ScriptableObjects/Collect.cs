bde2777564d9afb75d00e2c1929a026681bda715
Parker <10714265@my.uvu.edu>
2020-12-03 16:32:03 -0700

    Collect List

---
+++ UnityProject2/Assets/ScriptableObjects/Collect.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu]
+public class Collect : ScriptableObject
+{
+    public float ATKlvl = 1f;
+    public int Goldlvl = 10;
+    public Sprite art;
+    public Color artcolor;
+}
+ 
---
+++ UnityProject2/Assets/Scripts/Collection.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collection : MonoBehaviour
+{
+    public List<Collect> CollectableList;
+
+    private void Start()
+    {
+        
+        
+        for (var i = 0; i < CollectableList.Count; i++)
+        {
+            var position = new Vector3(i*2,0,0);
+            var item = CollectableList[i];
+            var newItem = new GameObject(item.name);
+            newItem.transform.position = position;
+            var sprite = newItem.AddComponent<SpriteRenderer>();
+            sprite.sprite = item.art;
+            sprite.color = item.artcolor;
+        }
+    }
+}
