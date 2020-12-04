2c9daedca3bfc3e61f2a0cd81d1f0897acfc7a0e
Parker <10714265@my.uvu.edu>
2020-12-03 20:19:08 -0700

    Flip object

---
+++ UnityProject2/Assets/Scripts/FlipObject.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlipObject : MonoBehaviour
+{
+    public float rotateValue;
+    
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow & KeyCode.UpArrow) || Input.GetKey(KeyCode.A))
+        {
+            rotateValue = -60;
+        }
+        
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        {
+            rotateValue = +60;
+        }
+        transform.rotation = Quaternion.Euler(0, rotateValue, 0);
+    }
+}
