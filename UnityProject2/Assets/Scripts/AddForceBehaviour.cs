44c6342e02ac3be1563f920567884c24e8e9922d
Parker <10714265@my.uvu.edu>
2020-12-04 10:11:02 -0700

    Coroutine

---
+++ UnityProject2/Assets/Scripts/AddForceBehaviour.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Rigidbody))]
+public class AddForceBehaviour : MonoBehaviour
+{
+    private Rigidbody rigid;
+    public float holdTime = 3f;
+    public int counter = 10;
+    public Vector3 forces, forces2;
+    public bool canRun;
+    
+    private IEnumerator Start()
+    {
+        rigid = GetComponent<Rigidbody>();
+        while (counter > 0)
+        {
+            yield return new WaitForSeconds(holdTime);
+            rigid.AddForce(forces);
+            counter--;
+        }
+        
+        while (canRun)
+        {
+            yield return new WaitForSeconds(holdTime);
+            rigid.AddForce(forces2);
+        }
+    }
+
+    
+}
---
+++ UnityProject2/Assets/Scripts/CoRoutineBehaviour.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class CoRoutineBehaviour : MonoBehaviour
+{
+
+    public UnityEvent startEvent, repeatEvent, endEvent;
+    public float holdTime, repeatholdTime=0.25f;
+    public int counter = 3;
+
+    public void runCoroutine()
+    {
+        StartCoroutine(Coroutine());
+    }
+
+    private IEnumerator Coroutine()
+    {
+        startEvent.Invoke();
+        
+        yield return new WaitForSeconds(holdTime);
+        
+        while (counter > 0)
+        {
+            repeatEvent.Invoke();
+            yield return new WaitForSeconds(repeatholdTime);
+            counter--;
+        }
+        yield return new WaitForSeconds(holdTime);
+        endEvent.Invoke();
+    }
+}
