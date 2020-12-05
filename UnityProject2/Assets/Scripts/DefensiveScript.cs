b912e12e8c173d23159f0b6a410df734e55dcdd9
Parker <10714265@my.uvu.edu>
2020-12-04 22:56:45 -0700

    Defense Enum

---
+++ UnityProject2/Assets/Scripts/DefensiveScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu]
+public class DefensiveScript : ScriptableObject
+{
+    public enum Weapons
+    {
+        Shield,
+        Helmet,
+        Armor
+    }
+    
+    public Weapons CurrentDefense;
+
+    public void runcurrentdefense()
+    {
+        switch (CurrentDefense)
+        {
+            case Weapons.Shield:
+                break;
+            case Weapons.Helmet:
+                break;
+            case Weapons.Armor:
+                break;
+            
+        }
+    }
+
+}
+
