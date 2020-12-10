c6e64474878fa60520c6150605cd2af4bd461524
Parker <10714265@my.uvu.edu>
2020-12-10 12:02:23 -0700

    Animated Platform

---
+++ UnityProject2/Assets/Scripts/AttatchonTrigger.cs
@@ -0,0 +1,19 @@
+﻿
+using UnityEngine;
+
+public class AttatchonTrigger : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Ground"))
+        {
+            transform.parent = other.transform;
+        }
+        
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        transform.parent = null;
+    }
+}
