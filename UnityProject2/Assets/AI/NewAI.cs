07b3a2da6ec8bb804d372a54c60de18ad73af9b8
Parker <10714265@my.uvu.edu>
2020-12-10 17:31:33 -0700

    Spike System

---
+++ UnityProject2/Assets/AI/NewAI.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+
+public class NewAI : MonoBehaviour
+{
+    private NavMeshAgent buddy;
+    public Transform player;
+
+    private void Start()
+    {
+        buddy = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        buddy.destination = player.position;
+    }
+}
