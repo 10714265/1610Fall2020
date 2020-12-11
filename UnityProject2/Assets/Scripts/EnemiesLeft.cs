94fec2a0c8ac781e777a772bf33cd8cac117fd44
Parker <10714265@my.uvu.edu>
2020-12-10 22:47:28 -0700

    Collection

---
+++ UnityProject2/Assets/Scripts/EnemiesLeft.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WhileLoop : MonoBehaviour
+{
+    int enemiesRemaining = 10;
+    
+    
+    void Start ()
+    {
+        while(enemiesRemaining > 0)
+        {
+            Debug.Log ("Enemy Slain");
+            enemiesRemaining--;
+        }
+    }
+}
---
+++ UnityProject2/Assets/Scripts/EnemyNumber.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ForLoop : MonoBehaviour
+{
+    int numberEnemies = 3;
+    
+    
+    void Start ()
+    {
+        for(int i = 0; i < numberEnemies; i++)
+        {
+            Debug.Log("Enemy: " + i);
+        }
+    }
+}
