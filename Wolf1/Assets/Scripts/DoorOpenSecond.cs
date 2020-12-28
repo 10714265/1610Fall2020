64fb5ec4f7ef0026d2c7507e344b30c5265b4e15
Parker <10714265@my.uvu.edu>
2020-12-28 16:48:25 -0700

    Door Open/Close Two

---
+++ Wolf1/Assets/Scripts/DoorOpenSecond.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorOpenSecond : MonoBehaviour
+{
+    public GameObject theDoor;
+    //public AudioSource doorFX;
+
+    void OnTriggerEnter(Collider other)
+    {
+        //doorFX.Play();
+        theDoor.GetComponent<Animator>().Play("DoorOpenTwo");
+        this.GetComponent<BoxCollider>().enabled = false;
+        StartCoroutine(CloseDoor());
+    }
+
+    IEnumerator CloseDoor()
+    {
+        yield return new WaitForSeconds(5);
+        //doorFX.Play();
+        theDoor.GetComponent<Animator>().Play("DoorCloseTwo");
+        this.GetComponent<BoxCollider>().enabled = true;
+    }
+}
